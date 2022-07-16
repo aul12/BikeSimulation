5776e6a87f4a6605e2b65524c323566d8ad66b3c
Paul Nykiel <paul.nykiel@gmail.com>
2022-07-16 21:24:09 +0200

    Added option to use multiple fit files for CdA estimation

--- CdAEstimation.py
+++ CdAEstimation.py
@@ -32,15 +32,15 @@ def estimate_cda(ds: list, delta_hs: list, Ps: list, ts: list, params):
 
 def main():
     parser = argparse.ArgumentParser("Estimate the CdA from a .fit File")
-    parser.add_argument("file", metavar="F", type=str,
-                        help="The fit file, needs to include distance, power and altitude")
+    parser.add_argument("files", metavar="F", type=str, nargs="+",
+                        help="The fit files, needs to include distance, power and altitude")
     parser.add_argument("--params", dest="params", type=str, help="Params file", default="params.json")
     parser.add_argument("--resolution", dest="resolution", type=float, help="Required resolution of the CdA",
                         default=0.001)
     args = parser.parse_args()
 
     params = ParamReader.read_params(args.params)
-    ds, delta_hs, Ps, ts = FitReader.read_fit(args.file)
+
 
     segment_initial_vels = []
     segment_ds = []
@@ -49,24 +49,26 @@ def main():
     segment_ts = []
     initial_vel = 5
 
-    last_was_zero = True
-    for d, delta_h, P, t in zip(ds, delta_hs, Ps, ts):
-        if P > 50:
-            if last_was_zero:
-                segment_ds.append([d])
-                segment_delta_hs.append([delta_h])
-                segment_Ps.append([P])
-                segment_ts.append([t])
-                segment_initial_vels.append(initial_vel)
+    for file in args.files:
+        ds, delta_hs, Ps, ts = FitReader.read_fit(file)
+        last_was_zero = True
+        for d, delta_h, P, t in zip(ds, delta_hs, Ps, ts):
+            if P > 50:
+                if last_was_zero:
+                    segment_ds.append([d])
+                    segment_delta_hs.append([delta_h])
+                    segment_Ps.append([P])
+                    segment_ts.append([t])
+                    segment_initial_vels.append(initial_vel)
+                else:
+                    segment_ds[-1].append(d)
+                    segment_delta_hs[-1].append(delta_h)
+                    segment_Ps[-1].append(P)
+                    segment_ts[-1].append(t)
+                last_was_zero = False
             else:
-                segment_ds[-1].append(d)
-                segment_delta_hs[-1].append(delta_h)
-                segment_Ps[-1].append(P)
-                segment_ts[-1].append(t)
-            last_was_zero = False
-        else:
-            initial_vel = d / t
-            last_was_zero = True
+                initial_vel = d / t
+                last_was_zero = True
 
     data_points = []
     for ds, delta_hs, Ps, ts, initial_vel in zip(segment_ds, segment_delta_hs, segment_Ps, segment_ts,
